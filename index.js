bbef4d853c1109485a0d6366ee0e4cd760057cc7
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-27 02:55:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function theBeatlesPlay(musicians,instruments) {
-  for (var i = 0; i< musicians.length; i++)
+  for (var i = 0; i< musicians.length; i++) {
+    var result = `${musicians[i]} plays ${instruments}`
+  }
   
 }
